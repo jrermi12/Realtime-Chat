ea77a366ba1bbb93c8152612da76c3902f3db9be
Ermias Asmare <61870061+jrermi12@users.noreply.github.com>
2022-08-15 07:50:12 +0300

    implemented the broadcast when a user join and leave the chat chord
    implemented the broadcast when a user join and leave the chat chord that notifies the other users that a user had left or joined the chat chord

--- server.js
+++ server.js
@@ -1,7 +1,22 @@
 const express = require('express');
+const path = require('path');
+const http = require('http');
+const socketio = require('socket.io')
 
 const app = express();
+const server = http.createServer(app)
+const io = socketio(server);
+
+//Set Static Folder
+app.use(express.static(path.join(__dirname, 'public')));
+
+//run when a client connects 
+io.on('connection', socket => {
+    console.log("new web socket connection")
+
+    socket.emit("message", 'welcome to chat chord');
+})
 
 const PORT = 3000 || process.env.PORT;
 
-app.listen(PORT, ()=>console.log(`Server running on port ${PORT}`));
+server.listen(PORT, ()=>console.log(`Server running on port ${PORT}`));
